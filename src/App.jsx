90d6ed95f9f04d08385588f81b33a73653249d24
momin-riyadh <mominriyadh@gmail.com>
2024-03-08 22:06:09 +0600

    New project has been scafoled

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+
+
+    return (
+        <>
+            <h1 className="text-3xl font-bold">Alkarim</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias consequuntur ducimus earum facere harum incidunt iste maiores minima mollitia, nihil numquam officia optio qui recusandae soluta suscipit totam ullam. Tempora.</p>
+        </>
+    )
+}
+
+export default App
