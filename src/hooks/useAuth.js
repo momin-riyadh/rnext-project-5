7478bebb615012138a41900c33c84ea6e6d6d341
momin-riyadh <mominriyadh@gmail.com>
2024-03-09 22:56:41 +0600

    fix and deploy

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -3,7 +3,6 @@ import {AuthContext} from "../context/index.js";
 
 
 export const useAuth = () => {
-
     const {auth} = useContext(AuthContext);
     useDebugValue(auth, auth => auth?.user ? "User Logged In" : "User Logged out");
     return useContext(AuthContext);
