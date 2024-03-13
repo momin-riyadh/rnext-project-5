e686ef89afa20f6e30ab592af371179132809653
momin-riyadh <mominriyadh@gmail.com>
2024-03-13 14:44:25 +0600

    fix and deploy part-4

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,6 +1,31 @@
-import React from 'react';
+import {useAuth} from "../hooks/useAuth.js";
+import useAxios from "../hooks/useAxios.js";
+import {useEffect, useState} from "react";
 
-function ProfilePage(props) {
+
+function ProfilePage() {
+    const [user, setUser] = useState(null);
+    const [posts, setPosts] = useState([]);
+    const [loading, setLoading] = useState(null);
+    const [error, setError] = useState(null);
+
+    const {api} = useAxios();
+    const {auth} = useAuth();
+
+    useEffect(() => {
+        const fetchProfile = async () => {
+            try {
+                const response = await api.get(`http://localhost:3000/profile/${auth?.user?.id}`);
+
+                setUser(response?.data?.user);
+                setPosts(response?.data?.posts);
+            } catch (error) {
+                console.error(error)
+            }
+        }
+
+        fetchProfile();
+    }, []);
     return (
         <div>Profile Page</div>
     );
