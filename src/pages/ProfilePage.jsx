b819d92fac8e898c1bd2f90a284e726b471bd481
momin-riyadh <mominriyadh@gmail.com>
2024-03-13 15:06:25 +0600

    fix and deploy part-4

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -6,13 +6,14 @@ import {useEffect, useState} from "react";
 function ProfilePage() {
     const [user, setUser] = useState(null);
     const [posts, setPosts] = useState([]);
-    const [loading, setLoading] = useState(null);
+    const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
 
     const {api} = useAxios();
     const {auth} = useAuth();
 
     useEffect(() => {
+        setLoading(true);
         const fetchProfile = async () => {
             try {
                 const response = await api.get(`http://localhost:3000/profile/${auth?.user?.id}`);
@@ -21,13 +22,24 @@ function ProfilePage() {
                 setPosts(response?.data?.posts);
             } catch (error) {
                 console.error(error)
+                setError(error)
+            } finally {
+                setLoading(false);
             }
         }
 
         fetchProfile();
     }, []);
+
+    if (loading) {
+        return <div>Fetching your profile data...</div>
+    }
+
     return (
-        <div>Profile Page</div>
+        <div>
+            Welcome, {user?.firstName}{' '}{user?.lastName}
+           <p>You have {posts.length} posts</p>
+        </div>
     );
 }
 
