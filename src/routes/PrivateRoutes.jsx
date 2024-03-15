9060471b98202390e311eff2292d13c8793b4961
momin-riyadh <mominriyadh@gmail.com>
2024-03-15 10:29:48 +0600

    fix and deploy part-4

--- src/routes/PrivateRoutes.jsx
+++ src/routes/PrivateRoutes.jsx
@@ -1,21 +1,22 @@
 import {useAuth} from "../hooks/useAuth.js";
 import {Navigate, Outlet} from "react-router-dom";
 import Header from "../components/common/Header.jsx";
+import ProfileProvider from "../providers/ProfileProvider.jsx";
 
 const PrivateRoutes = () => {
     const {auth} = useAuth();
     return (
         <div>
             {
-                auth.user ? (
-                    <>
+                auth.authToken ? (
+                    <ProfileProvider>
                         <Header/>
                         <main className="mx-auto max-w-[1020px] py-8">
                             <div className="container">
                                 <Outlet/>
                             </div>
                         </main>
-                    </>
+                    </ProfileProvider>
 
                 ) : (
                     <Navigate to="/login"/>
