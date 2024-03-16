5acd1eace24eb3ba84316f9957207e34c3b5a8b5
momin-riyadh <mominriyadh@gmail.com>
2024-03-16 13:04:36 +0600

    fix and deploy part-4

--- src/components/profile/ProfileImage.jsx
+++ src/components/profile/ProfileImage.jsx
@@ -8,6 +8,12 @@ function ProfileImage() {
     const {state, dispatch} = useProfile();
     const api = useAxios();
     const fileUploadRef = useRef();
+
+    const handleImageUpload = (event) => {
+        event.preventDefault()
+        fileUploadRef.current.click();
+    }
+
     return (
         <div
             className="relative mb-8 max-h-[180px] max-w-[180px] rounded-full lg:mb-11 lg:max-h-[218px] lg:max-w-[218px]"
@@ -17,14 +23,16 @@ function ProfileImage() {
                 src={`${import.meta.env.VITE_SERVER_BASE_URL}/${state?.user?.avatar}`}
                 alt={state?.user?.firstName}
             />
-            <form action="">
-                <input type="file" name="" id="" hidden/>
+            <form>
+                <button type="submit"
+                        className="flex-center absolute bottom-4 right-4 h-7 w-7 rounded-full bg-black/50 hover:bg-black/80"
+                        onClick={handleImageUpload}
+                >
+                    <img src={EDIT} alt="Edit"/>
+                </button>
+                <input ref={fileUploadRef} type="file" name="" id="" hidden/>
             </form>
-            <button
-                className="flex-center absolute bottom-4 right-4 h-7 w-7 rounded-full bg-black/50 hover:bg-black/80"
-            >
-                <img src={EDIT} alt="Edit"/>
-            </button>
+
         </div>
     );
 }
