a54f2197c1576324f59a14c7316bbec6144157a0
momin-riyadh <mominriyadh@gmail.com>
2024-03-16 13:37:50 +0600

    fix and deploy part-4

--- src/components/profile/ProfileImage.jsx
+++ src/components/profile/ProfileImage.jsx
@@ -7,7 +7,7 @@ import {actions} from "../../actions/index.js";
 
 function ProfileImage() {
     const {state, dispatch} = useProfile();
-    const api = useAxios();
+    const {api} = useAxios();
     const fileUploadRef = useRef();
 
     const handleImageUpload = (event) => {
@@ -41,11 +41,11 @@ function ProfileImage() {
             className="relative mb-8 max-h-[180px] max-w-[180px] rounded-full lg:mb-11 lg:max-h-[218px] lg:max-w-[218px]"
         >
             <img
-                className="max-w-full rounded-full w-52 h-52"
+                className="max-w-full rounded-full w-42 h-42"
                 src={`${import.meta.env.VITE_SERVER_BASE_URL}/${state?.user?.avatar}`}
                 alt={state?.user?.firstName}
             />
-            <form>
+            <form id="form" encType="multipart/form-data">
                 <button type="submit"
                         className="flex-center absolute bottom-4 right-4 h-7 w-7 rounded-full bg-black/50 hover:bg-black/80"
                         onClick={handleImageUpload}
