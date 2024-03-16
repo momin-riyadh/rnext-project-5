c2ea3f659a1a2d7ddb27c0fc7b887d4a9da468d3
momin-riyadh <mominriyadh@gmail.com>
2024-03-16 12:54:42 +0600

    fix and deploy part-4

--- src/components/profile/ProfileImage.jsx
+++ src/components/profile/ProfileImage.jsx
@@ -1,14 +1,13 @@
-
 import EDIT from '../../assets/icons/edit.svg'
 import {useProfile} from "../../hooks/useProfile.js";
 import useAxios from "../../hooks/useAxios.js";
-
-
+import {useRef} from "react";
 
 
 function ProfileImage() {
     const {state, dispatch} = useProfile();
     const api = useAxios();
+    const fileUploadRef = useRef();
     return (
         <div
             className="relative mb-8 max-h-[180px] max-w-[180px] rounded-full lg:mb-11 lg:max-h-[218px] lg:max-w-[218px]"
@@ -18,7 +17,9 @@ function ProfileImage() {
                 src={`${import.meta.env.VITE_SERVER_BASE_URL}/${state?.user?.avatar}`}
                 alt={state?.user?.firstName}
             />
-
+            <form action="">
+                <input type="file" name="" id="" hidden/>
+            </form>
             <button
                 className="flex-center absolute bottom-4 right-4 h-7 w-7 rounded-full bg-black/50 hover:bg-black/80"
             >
