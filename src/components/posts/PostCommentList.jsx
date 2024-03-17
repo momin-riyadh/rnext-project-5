a3f2f12a2ae268b2e27815e5748f65c4cd82ab37
momin-riyadh <mominriyadh@gmail.com>
2024-03-17 14:04:56 +0600

    fix and deploy part-5

--- src/components/posts/PostCommentList.jsx
+++ src/components/posts/PostCommentList.jsx
@@ -1,65 +1,29 @@
-export const PostCommentList = () => {
+const PostCommentList = ({ comments }) => {
     return (
-        <div>
-            {/*comment input box*/}
-            <div className="flex-center mb-3 gap-2 lg:gap-4">
-                <img
-                    className="max-w-7 max-h-7 rounded-full lg:max-h-[34px] lg:max-w-[34px]"
-                    src="./assets/images/avatars/avatar_1.png"
-                    alt="avatar"
-                />
-
-                <div className="flex-1">
-                    <input
-                        type="text"
-                        className="h-8 w-full rounded-full bg-lighterDark px-4 text-xs focus:outline-none sm:h-[38px]"
-                        name="post"
-                        id="post"
-                        placeholder="What's on your mind?"
-                    />
-                </div>
-            </div>
-            {/*comment filter button*/}
-            <div className="mt-4">
-                <button className="text-gray-300 max-md:text-sm">
-                    All Comment ▾
-                </button>
-            </div>
-            {/*comments */}
-            <div className="space-y-4 divide-y divide-lighterDark pl-2 lg:pl-3">
-                {/*single comment*/}
-                <div className="flex items-center gap-3 pt-4">
-                    <img
-                        className="max-w-6 max-h-6 rounded-full"
-                        src="./assets/images/avatars/avatar_2.png"
-                        alt="avatar"
-                    />
-                    <div>
-                        <div className="flex gap-1 text-xs lg:text-sm">
-                            <span>Tapas Adhikari: </span>
-                            <span>Great Sumit Saha dada ❤</span>
+        <div className="space-y-4 divide-y divide-lighterDark pl-2 lg:pl-3">
+            {comments &&
+                comments.map((comment) => (
+                    <div
+                        className="flex items-center gap-3 pt-4"
+                        key={comment.id}
+                    >
+                        <img
+                            className="max-w-6 max-h-6 rounded-full"
+                            src={`${import.meta.env.VITE_SERVER_BASE_URL}/${
+                                comment?.author?.avatar
+                            }`}
+                            alt="avatar"
+                        />
+                        <div>
+                            <div className="flex gap-1 text-xs lg:text-sm">
+                                <span>{comment?.author?.name}: </span>
+                                <span>{comment.comment}</span>
+                            </div>
                         </div>
                     </div>
-                </div>
-                {/*single comment ends */}
-
-                {/*single comment */}
-                <div className="flex items-center gap-3 pt-4">
-                    <img
-                        className="max-w-6 max-h-6 rounded-full"
-                        src="./assets/images/avatars/avatar_1.png"
-                        alt="avatar"
-                    />
-                    <div>
-                        <div className="flex gap-1 text-xs lg:text-sm">
-                            <span>Sumit Saha: </span>
-                            <span>Great Sumit Saha dada ❤</span>
-                        </div>
-                    </div>
-                </div>
-                {/*single comment ends*/}
-            </div>
-            {/*comments ends*/}
+                ))}
         </div>
-    )
-}
+    );
+};
+
+export default PostCommentList;
