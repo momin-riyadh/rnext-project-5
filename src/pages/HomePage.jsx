444d7dc2948c0c3d597c44e44967c5948a226440
Momin Riyadh <mominriyadh@gmail.com>
2024-03-21 15:57:37 +0600

    fix and deploy part-6

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,16 +1,43 @@
-import {useAuth} from "../hooks/useAuth.js";
-import {Link} from "react-router-dom";
+import {useReducer, useEffect} from "react";
+import {postReducer, initialState} from "../reducers/PostReducre.jsx";
+import useAxios from "../hooks/useAxios.js";
+import PostList from "../components/posts/PostList.jsx";
+import {actions} from "../actions/index.js";
 
 
 function HomePage() {
-    const {auth} = useAuth();
-    console.log(auth);
+
+    const [state, dispatch] = useReducer(postReducer, initialState);
+    const {api} = useAxios();
+
+    useEffect(() => {
+        dispatch({type: actions.post.DATA_FETCHING});
+        const fetchPost = async () => {
+            try {
+                const response = await api.get(`${import.meta.env.VITE_SERVER_BASE_URL}/posts`);
+                if (response.status === 200) {
+                    dispatch({type: actions.post.DATA_FETCHED, data: response.data})
+                }
+            } catch (error) {
+                console.log(error);
+                dispatch({type: actions.post.DATA_FETCH_ERROR, error: error.message})
+
+            }
+        }
+
+        fetchPost()
+    }, [])
+    if (state?.loading) {
+        return <div>We are working...</div>
+    }
+    if (state?.error) {
+        return <div>Error in fetching posts {state?.error?.message}</div>
+    }
+
     return (
         <>
-            <p>Homepage</p>
-            <Link to="me">Go to Profile Page</Link>
+            <PostList posts={state?.post}/>
         </>
-
 
     );
 }
